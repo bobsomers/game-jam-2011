d3fb180449ebdf98d25d11e6c447df0f3dbc0ecd
Bob Somers <bob@bobsomers.com>
2011-01-28 22:24:40 -0800

    Thruster stuff. Heyo.

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+require "hump.vector"
+
+local vector = hump.vector
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 ARENA_WIDTH = 800
@@ -10,7 +14,12 @@ walls = {
     bottom = {}
 }
 
-dino = {}
+dino = {
+    THRUSTER_DIST = 20,
+    head = {},
+    foot = {},
+    torso = {}
+}
 
 function love.load()
     local gfx = love.graphics
@@ -29,9 +38,35 @@ function love.load()
 	walls.top.shape = phys.newRectangleShape(walls.top.body, 0, 0, ARENA_WIDTH, 5, 0)
 	walls.bottom.body = phys.newBody(world, ARENA_WIDTH / 2, ARENA_HEIGHT - 2, 0, 0)
 	walls.bottom.shape = phys.newRectangleShape(walls.bottom.body, 0, 0, ARENA_WIDTH, 5, 0)
+    
+    dino.torso.body = phys.newBody(world, 400, ARENA_HEIGHT - 200, 10, 15)
+    dino.torso.shape = phys.newRectangleShape(dino.torso.body, 0, 0, 100, 50, 0)
+    dino.thrusterL = vector.new(dino.torso.body:getX() - dino.THRUSTER_DIST, dino.torso.body:getY())
+    dino.thrusterR = vector.new(dino.torso.body:getX() + dino.THRUSTER_DIST, dino.torso.body:getY())
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("a") or love.keyboard.isDown("d") then
+        -- calculate thruster positions and swap if necessary
+        dino.thrusterL = vector.new(dino.torso.body:getX() - dino.THRUSTER_DIST * math.cos(dino.torso.body:getAngle()),
+                                    dino.torso.body:getY() - dino.THRUSTER_DIST * math.sin(dino.torso.body:getAngle()))
+        dino.thrusterR = vector.new(dino.torso.body:getX() + dino.THRUSTER_DIST * math.cos(dino.torso.body:getAngle()),
+                                    dino.torso.body:getY() + dino.THRUSTER_DIST * math.sin(dino.torso.body:getAngle()))
+        if dino.thrusterL.x > dino.thrusterR.x then
+            dino.thrusterL, dino.thrusterR = dino.thrusterR, dino.thrusterL
+        end
+        
+        -- "d" controls the left thruster
+        if love.keyboard.isDown("d") then
+            dino.torso.body:applyForce(25, -100, dino.thrusterL.x, dino.thrusterL.y)
+        end
+        
+        -- "a" controls the right thruster
+        if love.keyboard.isDown("a") then
+            dino.torso.body:applyForce(-25, -100, dino.thrusterR.x, dino.thrusterR.y)
+        end
+    end
+    
     world:update(dt)
 end
 
@@ -51,4 +86,16 @@ function love.draw()
     drawSimpleRect(walls.right)
     drawSimpleRect(walls.top)
     drawSimpleRect(walls.bottom)
+    
+    gfx.push()
+    gfx.translate(dino.torso.body:getX(), dino.torso.body:getY())
+    gfx.rotate(dino.torso.body:getAngle())
+    gfx.setColor(255, 0, 0)
+    gfx.rectangle("fill", -50, -25, 100, 50)
+    gfx.pop()
+    
+    gfx.setColor(0, 255, 0)
+    gfx.point(dino.thrusterL.x, dino.thrusterL.y)
+    gfx.setColor(0, 0, 255)
+    gfx.point(dino.thrusterR.x, dino.thrusterR.y)
 end
