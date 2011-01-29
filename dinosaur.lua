c433cc6bae36d81b8c7965c632008b877a2a2aca
Chris Gibson <cgibson@mrvoxel.com>
2011-01-29 13:20:03 -0800

    Added tails, caused issues.  Yay!

--- dinosaur.lua
+++ dinosaur.lua
@@ -28,10 +28,26 @@ function Dinosaur.initTorso(self, x, y)
     self.thrusterL = vector.new(dino.torso.body:getX() - dino.THRUSTER_DIST, dino.torso.body:getY())
     self.thrusterR = vector.new(dino.torso.body:getX() + dino.THRUSTER_DIST, dino.torso.body:getY())
 
-    self.foot.body = phys.newBody(world, x, y - 25, 5, 7)
+    self.foot.body = phys.newBody(world, x, y + 25, 2, 3)
     self.foot.shape = phys.newRectangleShape(dino.foot.body, 0, 0, 50, 50, 0)
 
-    joint = love.physics.newDistanceJoint( self.torso.body, self.foot.body, x, y, x, y-25 )
+    --joint = love.physics.newDistanceJoint( self.torso.body, self.foot.body, x+25, y, x, y+25 )
+    --joint2 = love.physics.newDistanceJoint( self.torso.body, self.foot.body, x-25, y, x, y+25 )
+    --joint = love.physics.newWeldJoint( self.torso.body, self.foot.body )
+
+    self.tail = {}
+
+    for i=1,1 do
+        self.tail[i] = {}
+        self.tail[i].body = phys.newBody(world, x + 50 + (i * 15), y, 0.000001, 0.000001)
+        self.tail[i].shape = phys.newRectangleShape(self.tail[i].body, 0, 0, 20 - 2*i, 20 - 2*i, 0)
+
+        if i > 1 then
+            --self.tail[i].joint = love.physics.newRevoluteJoint( self.tail[i-1].body, self.tail[i].body, x + 50 + ((i-1) * 15), y )
+        else
+            --self.tail[i].joint = love.physics.newRevoluteJoint( self.torso.body, self.tail[i].body, x + 50 + ((i-1) * 15), y )
+        end
+    end
 
 end
 
@@ -60,6 +76,17 @@ function Dinosaur.draw(self)
         gfx.setColor(0, 255, 0)
         gfx.rectangle("fill", -25, -25, 50, 50)
         gfx.pop()
+
+    for i=1,1 do
+        gfx.push()
+        gfx.translate(self.tail[i].body:getX(), self.tail[i].body:getY())
+        gfx.rotate(self.tail[i].body:getAngle())
+        gfx.setColor(0, 0, 255)
+        --gfx.rectangle("fill", -5, -5, 20 - 2*i, 20 - 2*i)
+        gfx.setColor(255, 255, 255)
+        gfx.draw(resources["tail01"], 0, 0, 0, 2, 2, -5, -5)
+        gfx.pop()
+    end
 
 
 end
