dc07efb7550ee82aaa5544eb3c14cd9c3693194b
Bob Somers <bob@bobsomers.com>
2011-01-30 14:58:42 -0800

    Titles.

--- main.lua
+++ main.lua
@@ -18,6 +18,12 @@ PLAY_TIME = 60
 function love.load()
     gfx.setBackgroundColor(0, 0, 0)
     
+    -- create titles
+    cpgd = gfx.newImage("title/cpgd.png")
+    deadpixel = gfx.newImage("title/deadpixel.png")
+    esrb = gfx.newImage("title/esrb.png")
+    title = gfx.newImage("title/title.png")
+    
     -- create world
     world = phys.newWorld(0, 0, ARENA_WIDTH, ARENA_HEIGHT)
     world:setCallbacks(physadd, nil, nil, nil)
@@ -69,7 +75,7 @@ function love.load()
     
     -- game state and scoring
     gameStartTime = love.timer.getTime()
-    gameState = "playing"
+    gameState = "title"
     gameScore = 0
     
     -- ui schtuff
@@ -84,166 +90,179 @@ function love.load()
     jetpacksound:setLooping(true)
     boomsound = love.audio.newSource("sound/boom.wav", "static")
     launchsound = love.audio.newSource("sound/launch.wav", "static")
-    
-    love.audio.play(bgmusic)
 end
 
-function love.update(dt)    
-    -- update player
-    dino:update(dt)
-    
-    -- update camera
-    local velX, velY = dino.torso.body:getLinearVelocity()
-    local playerVel = vector.new(velX, velY)
-    local zoom = playerVel:len() / 1200
-    if zoom > 1 then zoom = 1 end
-    zoom = ((1 - zoom) / 2) + 0.5
-    pos = vector.new(dino.torso.body:getX(), dino.torso.body:getY() - 100)
-    cam.zoomV = cam.spring(dt, cam.zoom, zoom, cam.zoomV)
-    local dz = cam.zoomV * dt
-    cam.posV.x = cam.spring(dt, cam.pos.x, pos.x, cam.posV.x)
-    local dx = cam.posV.x * dt
-    cam.posV.y = cam.spring(dt, cam.pos.y, pos.y, cam.posV.y)
-    local dy = cam.posV.y * dt
-    cam.pos = vector.new(cam.pos.x + dx, cam.pos.y + dy)
-    cam.zoom = cam.zoom + dz
-    
-    -- update missiles
-    for k, v in pairs(missiles) do
-        if k ~= "id" then
-            v:update(dt)
-        end
-    end
-    
-    -- update lasers
-    for k, v in pairs(lasers) do
-        if k ~= "id" then
-            v:update(dt)
-        end
-    end
-    
-    -- update and cull explosions
-    for k, v in pairs(explosions) do
-        if k ~= "id" then
-            if love.timer.getTime() - v.spawnTime > 1000 then
-                v = nil
-            end
-            v:update(dt)
-        end
-    end
-    
-    -- update physics world
-    world:update(dt)
-    
-    -- time up?
-    elapsed = love.timer.getTime() - gameStartTime
-    if elapsed > PLAY_TIME then
-        gameState = "over"
-    end
-end
-
-function love.draw()    
-    -- draw background
-    local skyPos = vector.new(-cam.pos.x / 6, ((-cam.pos.y - 100) / 6) - 10)
-    gfx.draw(sky, skyPos.x, skyPos.y)
-
-    -- begin world drawing
-    cam:predraw()
-
-    -- draw terrain
-    for i = 1, 6 do
-        gfx.draw(sand, -192 + 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, -384 + 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, -576 + 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, -768 + 5, (i - 1) * 224, 0, 4, 4)
+function love.update(dt)
+    if gameState ~= "title" then
+        -- update player
+        dino:update(dt)
         
-        gfx.draw(sand, 2400 - 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, 2592 - 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, 2784 - 5, (i - 1) * 224, 0, 4, 4)
-        gfx.draw(sand, 2976 - 5, (i - 1) * 224, 0, 4, 4)
+        -- update camera
+        local velX, velY = dino.torso.body:getLinearVelocity()
+        local playerVel = vector.new(velX, velY)
+        local zoom = playerVel:len() / 1200
+        if zoom > 1 then zoom = 1 end
+        zoom = ((1 - zoom) / 2) + 0.5
+        pos = vector.new(dino.torso.body:getX(), dino.torso.body:getY() - 100)
+        cam.zoomV = cam.spring(dt, cam.zoom, zoom, cam.zoomV)
+        local dz = cam.zoomV * dt
+        cam.posV.x = cam.spring(dt, cam.pos.x, pos.x, cam.posV.x)
+        local dx = cam.posV.x * dt
+        cam.posV.y = cam.spring(dt, cam.pos.y, pos.y, cam.posV.y)
+        local dy = cam.posV.y * dt
+        cam.pos = vector.new(cam.pos.x + dx, cam.pos.y + dy)
+        cam.zoom = cam.zoom + dz
+        
+        -- update missiles
+        for k, v in pairs(missiles) do
+            if k ~= "id" then
+                v:update(dt)
+            end
+        end
+        
+        -- update lasers
+        for k, v in pairs(lasers) do
+            if k ~= "id" then
+                v:update(dt)
+            end
+        end
+        
+        -- update and cull explosions
+        for k, v in pairs(explosions) do
+            if k ~= "id" then
+                if love.timer.getTime() - v.spawnTime > 1000 then
+                    v = nil
+                end
+                v:update(dt)
+            end
+        end
+        
+        -- update physics world
+        world:update(dt)
+        
+        -- time up?
+        elapsed = love.timer.getTime() - gameStartTime
+        if elapsed > PLAY_TIME then
+            gameState = "over"
+        end
     end
-    for i = -2, 16 do
-        gfx.draw(grass, (i - 1) * 192, ARENA_HEIGHT - 5, 0, 4, 4)
-        gfx.draw(grass, (i - 1) * 192, ARENA_HEIGHT + 448 - 5, 0, 4, -4)
-    end
-    
-    -- draw walls
-    -- TODO: take this out
-    --for k,v in pairs(walls) do
-    --    v:draw()
-    --end
+end
 
-    dino:draw()
-
-    -- draw missiles
-    for k, v in pairs(missiles) do
-        if k ~= "id" then
-            v:draw()
+function love.draw()
+    if gameState == "title" then
+        elapsed = love.timer.getTime() - gameStartTime
+        if elapsed < 3 then
+            gfx.setColor(255, 255, 255, math.sin(math.pi / 3 * elapsed) * 255)
+            gfx.draw(cpgd, 0, 0, 0, 2.5, 2.5)
+        elseif elapsed >= 3 and elapsed < 6 then
+            gfx.setColor(255, 255, 255, math.sin(math.pi / 3 * (elapsed - 3)) * 255)
+            gfx.draw(deadpixel, 0, 0, 0, 2.5, 2.5)
+        elseif elapsed >= 6 and elapsed < 9 then
+            gfx.setColor(255, 255, 255, math.sin(math.pi / 3 * (elapsed - 6)) * 255)
+            gfx.draw(esrb, 0, 0, 0, 2.5, 2.5)
+        elseif elapsed >= 9 and elapsed < 10.5 then
+            gfx.setColor(255, 255, 255, math.sin(math.pi / 3 * (elapsed - 9)) * 255)
+            gfx.draw(title, 0, 0, 0, 2.5, 2.5)
+        else
+            gfx.draw(title, 0, 0, 0, 2.5, 2.5)
         end
-    end
-    
-    -- draw lasers
-    for k, v in pairs(lasers) do
-        if k ~= "id" then
-            v:draw()
-        end
-    end
-    
-    -- draw explosions
-    for k, v in pairs(explosions) do
-        if k ~= "id" then
-            v:draw()
-        end
-    end
-    
-    --[[
-    -- thruster debug drawing
-    gfx.setColor(0, 255, 0)
-    gfx.setLine(5, "smooth")
-    gfx.line(dino.thruster.left.pos.x, dino.thruster.left.pos.y, dino.thruster.left.pos.x - dino.thruster.left.dir.x, dino.thruster.left.pos.y - dino.thruster.left.dir.y)
-    gfx.setColor(0, 0, 255)
-    gfx.setLine(5, "smooth")
-    gfx.line(dino.thruster.right.pos.x, dino.thruster.right.pos.y, dino.thruster.right.pos.x - dino.thruster.right.dir.x, dino.thruster.right.pos.y - dino.thruster.right.dir.y)
-    --]]
-    
-    -- end world drawing
-    cam:postdraw()
-    
-    -- draw ui
-    gfx.setFont(infoFont)
-    gfx.setColor(0, 0, 0)
-    gfx.print("Time Remaining", 22, 7)
-    gfx.setColor(255, 255, 255)
-    gfx.print("Time Remaining", 20, 5)
-    gfx.setColor(0, 0, 0)
-    gfx.print("Kill Count", 22, 502)
-    gfx.setColor(255, 255, 255)
-    gfx.print("Kill Count", 20, 500)
-    
-    -- game timer and score
-    elapsed = love.timer.getTime() - gameStartTime
-    if elapsed > PLAY_TIME then
-        remaining = 0
     else
-        remaining = PLAY_TIME - elapsed
-    end
-    gfx.setFont(timerFont)
-    gfx.setColor(0, 0, 0)
-    gfx.print(string.format("%.2f", remaining), 22, 25)
-    gfx.setColor(255, 255, 255)
-    gfx.print(string.format("%.2f", remaining), 20, 23)
-    gfx.setColor(0, 0, 0)
-    gfx.print(gameScore, 22, 520)
-    gfx.setColor(255, 255, 255)
-    gfx.print(gameScore, 20, 518)
-    
-    -- game over text
-    if gameState == "over" then
-        gfx.setFont(overFont)
+        -- draw background
+        local skyPos = vector.new(-cam.pos.x / 6, ((-cam.pos.y - 100) / 6) - 10)
+        gfx.draw(sky, skyPos.x, skyPos.y)
+
+        -- begin world drawing
+        cam:predraw()
+
+        -- draw terrain
+        for i = 1, 6 do
+            gfx.draw(sand, -192 + 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, -384 + 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, -576 + 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, -768 + 5, (i - 1) * 224, 0, 4, 4)
+            
+            gfx.draw(sand, 2400 - 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, 2592 - 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, 2784 - 5, (i - 1) * 224, 0, 4, 4)
+            gfx.draw(sand, 2976 - 5, (i - 1) * 224, 0, 4, 4)
+        end
+        for i = -2, 16 do
+            gfx.draw(grass, (i - 1) * 192, ARENA_HEIGHT - 5, 0, 4, 4)
+            gfx.draw(grass, (i - 1) * 192, ARENA_HEIGHT + 448 - 5, 0, 4, -4)
+        end
+
+        dino:draw()
+
+        -- draw missiles
+        for k, v in pairs(missiles) do
+            if k ~= "id" then
+                v:draw()
+            end
+        end
+        
+        -- draw lasers
+        for k, v in pairs(lasers) do
+            if k ~= "id" then
+                v:draw()
+            end
+        end
+        
+        -- draw explosions
+        for k, v in pairs(explosions) do
+            if k ~= "id" then
+                v:draw()
+            end
+        end
+        
+        --[[
+        -- thruster debug drawing
+        gfx.setColor(0, 255, 0)
+        gfx.setLine(5, "smooth")
+        gfx.line(dino.thruster.left.pos.x, dino.thruster.left.pos.y, dino.thruster.left.pos.x - dino.thruster.left.dir.x, dino.thruster.left.pos.y - dino.thruster.left.dir.y)
+        gfx.setColor(0, 0, 255)
+        gfx.setLine(5, "smooth")
+        gfx.line(dino.thruster.right.pos.x, dino.thruster.right.pos.y, dino.thruster.right.pos.x - dino.thruster.right.dir.x, dino.thruster.right.pos.y - dino.thruster.right.dir.y)
+        --]]
+        
+        -- end world drawing
+        cam:postdraw()
+        
+        -- draw ui
+        gfx.setFont(infoFont)
         gfx.setColor(0, 0, 0)
-        gfx.print("GAME OVER", 202, 202)
+        gfx.print("Time Remaining", 22, 7)
         gfx.setColor(255, 255, 255)
-        gfx.print("GAME OVER", 200, 200)
+        gfx.print("Time Remaining", 20, 5)
+        gfx.setColor(0, 0, 0)
+        gfx.print("Score", 22, 502)
+        gfx.setColor(255, 255, 255)
+        gfx.print("Score", 20, 500)
+        
+        -- game timer and score
+        elapsed = love.timer.getTime() - gameStartTime
+        if elapsed > PLAY_TIME then
+            remaining = 0
+        else
+            remaining = PLAY_TIME - elapsed
+        end
+        gfx.setFont(timerFont)
+        gfx.setColor(0, 0, 0)
+        gfx.print(string.format("%.2f", remaining), 22, 25)
+        gfx.setColor(255, 255, 255)
+        gfx.print(string.format("%.2f", remaining), 20, 23)
+        gfx.setColor(0, 0, 0)
+        gfx.print(gameScore, 22, 520)
+        gfx.setColor(255, 255, 255)
+        gfx.print(gameScore, 20, 518)
+        
+        -- game over text
+        if gameState == "over" then
+            gfx.setFont(overFont)
+            gfx.setColor(0, 0, 0)
+            gfx.print("GAME OVER", 202, 202)
+            gfx.setColor(255, 255, 255)
+            gfx.print("GAME OVER", 200, 200)
+        end
     end
 end
 
@@ -258,7 +277,11 @@ function love.keyreleased(key, unicode)
 end
 
 function love.keypressed(key, unicode)
-    if gameState == "playing" then
+    if gameState == "title" then
+        gameState = "playing"
+        gameStartTime = love.timer.getTime()
+        love.audio.play(bgmusic)
+    elseif gameState == "playing" then
         if key == " " then
             dino:right()
         elseif key == "right" then
@@ -281,16 +304,7 @@ function love.keypressed(key, unicode)
             end
         end
     elseif gameState == "over" then
-        if key == "return" then
-            dino.torso.body:setPosition(1200, ARENA_HEIGHT - 200)
-            dino.torso.body:setAngle(0)
-            dino.torso.body:setLinearVelocity(0, 0)
-            dino.torso.body:setAngularVelocity(0, 0)
-            
-            cam.pos = vector.new(1200, ARENA_HEIGHT - 200)
-            
-            -- delete t-rexes?
-        end
+        -- nothing, lol, out of time
     end
 end
 
